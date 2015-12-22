317a7ad6b5efd4efbdcc94121c23cd4d8094f6a1
fyl080801 <fyl080801@hotmail.com>
2015-12-23 02:13:20 +0800

    老婆

--- ledtest2/src/erg/mpis/led102/Led102.java
+++ ledtest2/src/erg/mpis/led102/Led102.java
@@ -5,26 +5,24 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import com.sun.jna.Pointer;
-import com.sun.jna.platform.win32.BaseTSD.SSIZE_T;
-
 import erg.mpis.led102.LedInterface.LedContrl;
-
 
 /**
  * 10号线2期LED屏控制
+ * 
  * @author jiaoyongxin
  *
  */
 public class Led102 {
-	public static String ledip="10.0.0.18";
-	public static final short RET_PROCESS = 0;//回掉通知函数调用成功
-	public static final short RET_PARAM_ERROR = - 1;
-	public static final short RET_UNINIT = - 2;
-	public static final short RET_FAILUE = - 3;
-	public static final short RET_BUSY = - 4;
-	public static final short RET_NOMEM = - 5;
+	public static String ledip = "10.0.0.18";
+	public static final short RET_PROCESS = 0;// 回掉通知函数调用成功
+	public static final short RET_PARAM_ERROR = -1;
+	public static final short RET_UNINIT = -2;
+	public static final short RET_FAILUE = -3;
+	public static final short RET_BUSY = -4;
+	public static final short RET_NOMEM = -5;
 
-	//回调返回的状态值，通信结果
+	// 回调返回的状态值，通信结果
 	public static final short RESULT_OK = 0;
 	public static final short RESULT_REBOOT = 200;
 	public static final short RESULT_UNSUPPORT = 300;
@@ -35,43 +33,37 @@ public class Led102 {
 	public static final short RESULT_CONNECTFAIL = 305;
 	public static final short RESULT_ILLEGALCMM = 306;
 	public static final short RESULT_DATAERROR = 307;
-	public static final short RESULT_OTHERERROR = 500;	
-	//通信出错
-	public static final short RESULT_COMM_ERR = - 1;
-	  //接收的数据出错
-	public static final short RESULT_DATAREVERROR = - 2;
-	  //超时
-	public static final short RESULT_TIMEOUT = - 3;
-	  //停止
-    public static final short RESULT_STOP=-3;
-  
-	public static void main(String[] args) throws UnknownHostException,
-	IOException, InterruptedException {
+	public static final short RESULT_OTHERERROR = 500;
+	// 通信出错
+	public static final short RESULT_COMM_ERR = -1;
+	// 接收的数据出错
+	public static final short RESULT_DATAREVERROR = -2;
+	// 超时
+	public static final short RESULT_TIMEOUT = -3;
+	// 停止
+	public static final short RESULT_STOP = -3;
+
+	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		LedContrl led = LedInterface.getLedContrl();
 		if (led != null) {
 			System.out.println("动态链接库加载成功！");
 		}
-	
-		int timeout=6000;
-	    led.InitCommEnv(timeout);
-	    Thread.sleep(1000);
-	    byte[] cardip = InetAddress.getByName(ledip).getAddress();
-		//  char[] cardip = new String("addr").toCharArray();
-        led.SelectNetChanel(cardip);
-        LedInterface.ResultCB cb = new ResultCBExecute();
-        Pointer userdata=null;
-     
- 
-        led.SetDefaultLight(1, cb, userdata);
-        led.SetDefaultLight(22, cb, userdata);
-        led.PowerOffLed(cb, userdata);
-        led.PowerOnLed(cb, userdata);
-        led.ReleaseCommEnv();
-}
 
+		int timeout = 6000;
+		led.InitCommEnv(timeout);
+		Thread.sleep(1000);
+		byte[] cardip = InetAddress.getByName(ledip).getAddress();
+		// char[] cardip = new String("addr").toCharArray();
+		led.SelectNetChanel(cardip);
+		LedInterface.ResultCB cb = new ResultCBExecute();
+		Pointer userdata = null;
 
-	
-	
-	
+		led.SetDefaultLight(1, cb, userdata);
+		led.SetDefaultLight(22, cb, userdata);
+		led.PowerOffLed(cb, userdata);
+		led.PowerOnLed(cb, userdata);
+		led.ReleaseCommEnv();
 	}
+
+}
--- ledtest2/src/erg/mpis/led102/LedInterface.java
+++ ledtest2/src/erg/mpis/led102/LedInterface.java
@@ -1,30 +1,18 @@
 package erg.mpis.led102;
 
-import java.util.Arrays;
-import java.util.List;
-
-//import erg.mpis.led102.LedInterface.LedContrl.PBrightLevelinfo;
-
 import com.sun.jna.Library;
 import com.sun.jna.Native;
-import com.sun.jna.NativeLibrary;
 import com.sun.jna.Pointer;
-import com.sun.jna.Structure;
-import com.sun.jna.platform.win32.WinDef.HWND;
-import com.sun.jna.Structure;
-import com.sun.jna.platform.win32.OaIdl.VARDESC;
-import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.win32.StdCallLibrary.StdCallCallback;
 
 public class LedInterface {
 
 	public static LedContrl getLedContrl() {
-		NativeLibrary.addSearchPath("interface", "D:\\eclipse64\\workspace1");
-	//	NativeLibrary.addSearchPath("LedContrl.class",
-	//			"D:\\eclipse64\\workspace1");
-		LedContrl led = (LedContrl) Native.loadLibrary("interface",
-				LedContrl.class);
-		return led;
+		// NativeLibrary.addSearchPath("LedContrl", "\\");
+		// NativeLibrary.addSearchPath("LedContrl.class",
+		// "D:\\eclipse64\\workspace1");
+		// NativeLibrary.addSearchPath("", "/");
+		return (LedContrl) Native.loadLibrary("interface.dll", LedContrl.class);
 	}
 
 	public interface ResultCB extends StdCallCallback {
@@ -33,23 +21,24 @@ public class LedInterface {
 	}
 
 	public interface LedContrl extends Library {
-// PowerOnLed
+		// PowerOnLed
 		public int InitCommEnv(int timeout);
 
 		public int SelectNetChanel(byte[] cardip);
-		
+
 		public int PowerOnLed(ResultCB cb, Pointer userdata);
-		
+
 		public int PowerOffLed(ResultCB cb, Pointer userdata);
-		
-	//	public int SetLightLevel(PBrightLevelinfo brightarray,ResultCB cb,Pointer userdata);//LED 亮度设置
-		
-		public int SetDefaultLight(int level,ResultCB cb, Pointer userdata);//LED 默认亮度
-		
-		public int SetTmpLightLevel(int levle,ResultCB cb, Pointer userdat);//临时调节LED亮度
+
+		// public int SetLightLevel(PBrightLevelinfo brightarray,ResultCB
+		// cb,Pointer userdata);//LED 亮度设置
+
+		public int SetDefaultLight(int level, ResultCB cb, Pointer userdata);// LED
+																				// 默认亮度
+
+		public int SetTmpLightLevel(int levle, ResultCB cb, Pointer userdat);// 临时调节LED亮度
 
 		public int ReleaseCommEnv();
 	}
 
-	
 }
